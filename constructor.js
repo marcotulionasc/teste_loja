b048f75de1e87fae5c9174a1f21608010b5e3a77
marcotulionasc <marcos.tulio.santana@ibm.com>
2023-07-04 12:30:02 -0300

    header js

--- constructor.js
+++ constructor.js
@@ -1,3 +1,10 @@
 var titulo_produto = "Titulo Produto";
 
 var descricao_produto = "Descricao Produto";
+
+var header_produto = "Titulo de apresentação do produto";
+
+var item1 = "Titulo de apresentação do produto";
+var item2 = "Titulo de apresentação do produto";
+var item3 = "Titulo de apresentação do produto";
+var item4 = "Titulo de apresentação do produto";
