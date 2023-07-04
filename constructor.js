963fee9416eeb102444e78d7748c2f3c9cee67de
marcotulionasc <marcos.tulio.santana@ibm.com>
2023-07-04 20:19:53 -0300

    teste mudar

--- constructor.js
+++ constructor.js
@@ -1,15 +1,15 @@
-var titulo_produto = "Titulo Produto";
+var titulo_produto = "TESTE";
 
-var descricao_produto = "Descricao Produto";
+var descricao_produto = "TESTE";
 
-var header_produto = "Titulo de apresentação do produto";
+var header_produto = "TESTE";
 
-var item1_titulo = "Item 1 - titulo";
-var item2_titulo = "Item 2 - titulo";
-var item3_titulo = "Item 3 - titulo";
-var item4_titulo = "Item 4 - titulo";
+var item1_titulo = "TESTE";
+var item2_titulo = "TESTE";
+var item3_titulo = "TESTE";
+var item4_titulo = "TESTE";
 
-var item1_p = "Item 1 - corpo";
-var item2_p = "Item 2 - corpo";
-var item3_p = "Item 3 - corpo";
-var item4_p = "Item 4 - corpo";
+var item1_p = "TESTE";
+var item2_p = "TESTE";
+var item3_p = "TESTE";
+var item4_p = "TESTE";
