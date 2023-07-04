0ea85d52b54417a8d63512dc571a0f6007e787fc
marcotulionasc <marcos.tulio.santana@ibm.com>
2023-07-04 12:38:15 -0300

    nao vou ficar broke

--- constructor.js
+++ constructor.js
@@ -4,7 +4,12 @@ var descricao_produto = "Descricao Produto";
 
 var header_produto = "Titulo de apresentação do produto";
 
-var item1 = "Titulo de apresentação do produto";
-var item2 = "Titulo de apresentação do produto";
-var item3 = "Titulo de apresentação do produto";
-var item4 = "Titulo de apresentação do produto";
+var item1_titulo = "Item 1 - titulo";
+var item2_titulo = "Item 2 - titulo";
+var item3_titulo = "Item 3 - titulo";
+var item4_titulo = "Item 4 - titulo";
+
+var item1_p = "Item 1 - corpo";
+var item2_p = "Item 2 - corpo";
+var item3_p = "Item 3 - corpo";
+var item4_p = "Item 4 - corpo";
